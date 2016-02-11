58aa3115976addcd0d74b879b8347b6c174faf04
Yanni-Psitos <ypsitos@gmail.com>
2016-02-11 16:37:31 -0500

    Official Completed Code, with all database items, favorites, etc.

--- NeighborhoodApp/app/src/main/java/ypsitos/neighborhoodapp/SQLiteOpenHelper.java
+++ NeighborhoodApp/app/src/main/java/ypsitos/neighborhoodapp/SQLiteOpenHelper.java
@@ -48,11 +48,44 @@ public class SQLiteOpenHelper extends android.database.sqlite.SQLiteOpenHelper {
         db.execSQL(CREATE_HERO_LIST_TABLE);
         String deadpoolRow = "INSERT INTO " + TABLE_NAME + " ("
                 + COL_ID+", " + COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
-                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Deadpool', 'Wade Wilson', 'Accelerated Healing Factor, Trained Assassin, Breaks the 4th Wall, Chimichangas', 'Unlike Wolverine’s natural healing factor, Deadpool’s is mentally driven to a partial extent. Due to the presence of this superhuman healing ability, many of Deadpool’s natural physical attributes have been enhanced. Deadpool’s musculature generates considerably less fatigue toxins than the muscles of an ordinary human being, granting him superhuman levels of stamina in all physical activities. His natural strength, agility and reflexes have been enhanced to levels that are beyond the natural limits of the human body. Deadpool’s agility and reaction time are superior to those of even the finest human athlete.')";
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Deadpool', 'Wade Wilson', 'Accelerated Healing Factor, Trained Assassin, Marksman, Martial Artist, Enhanced Reflexes and Endurance, Breaks the 4th Wall, Chimichangas', 'Unlike Wolverine’s natural healing factor, Deadpool’s is mentally driven to a partial extent. Due to the presence of this superhuman healing ability, many of Deadpool’s natural physical attributes have been enhanced. Deadpool’s musculature generates considerably less fatigue toxins than the muscles of an ordinary human being, granting him superhuman levels of stamina in all physical activities. His natural strength, agility and reflexes have been enhanced to levels that are beyond the natural limits of the human body. Deadpool’s agility and reaction time are superior to those of even the finest human athlete.')";
         String wolverineRow = "INSERT INTO " + TABLE_NAME + " ("
                 + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
                 + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Wolverine','Logan Howlett','Accelerated Healing Factor, Retractable Claws, Adamantium Skeleton','Due to his extensive training as a soldier, a C.I.A. operative, a samurai, a spy, and a member of the X-Men, Wolverine is an exceptional hand-to-hand combatant, having mastered virtually every fighting style on Earth. He is also a trained expert in multiple types of weapons, vehicles, computer systems, explosives, and assassination techniques.')";
+        String jokerRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Joker','Jack Napier','Insanity, Gadgets, Tricks','His crimes require no motivation other than his sadistic desire to show people the meaninglessness of life through pain and death, and the narcissism to see the world remade in his own image. He is constantly adapting his personality and his psychosis to respond to the world around him, sometimes a harmless trickster and at others a brutal mass-murderer.')";
+        String batmanRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Batman','Bruce Wayne','Trained Martial Artist, Inexhaustible Wealth, Brilliant Deductive Skill, Gadgets','As a polymath, his knowledge and expertise in almost every discipline known to man is nearly unparalleled. Batman has been repeatedly described as having a genius-level intellect, one of the greatest martial artists in the DC Universe, and having peak human physical conditioning.')";
+        String hulkRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Hulk','Bruce Banner','Incredible Strength, High Physical Damage Resistance, Healing Factor, Intellectual Genius as Bruce','Bruce was exposed to extreme amounts of gamma radiation that altered his DNA structure due to gamma rays and trauma passed down by his Father and caused him to become a giant green (at first grey) monster of incredible power whenever he starts to get angry.')";
+        String kickassRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Kick-Ass','David Lizewski','Immunity To Pain, Martial Artist, Partial Metal Implants','After his first unsuccessful attempt at superheroics ends in being beaten, mugged and hit by a moving car, he has several metal plates and braces surgically implanted within his body during the course of his hospitalization and recovery. This, along with the nerve damage he suffers during the event, provides him with the ability to be almost immune to pain, giving him an edge while fighting. His only weapons are his twin batons that are wrapped in green electrical tape.')";
+        String thorRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Thor','Thor Odinson','Incredible Strength, Long Lifespan, High Physical Damage Resistance, Storm Calling, Dimensional Travel, Flight','As the Norse god of thunder, Thor can summon the elements of the storm (lightning; rain; wind; snow) and uses Mjolnir as a tool to focus this ability, although the hammer cannot command artificial weather, only natural. He can cause these weather effects over the world and destroy entire buildings; by whirling his hammer he can lift entire buildings with the wind. Thor can also create small tornadoes by quickly whipping his cape in circles.As the son of the Earth goddess Gaea, Thor has shown some control over the Earth.')";
+        String antmanRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Ant-Man','Scott Lang','Change Size At Will, Telepathic Communication With Insects, Subatomic Travel, Electronics Expertise','Using a gaseous form of Pym particles kept in a compartment in his belt, Ant-Man had the power to shrink himself (and other people and objects along with himself) to the size of an ant and return to normal. Over time, he has acquired the ability to change size at will, seemingly without the need for the gas.')";
+        String gokuRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Goku','Kakarot','Incredible Strength, Extreme Speed And Reflexes, Flight, Teleportation, Harnessing Of Ki Energy, Super Saiyan Transformations','Goku can generate energy blasts from an inner energy called ki, notably in the form of the Kamehameha Wave. He can teleport anywhere he wants instantly via Instant Transmission. He is capable of unaided flight, and was capable of aided flight through the use of a small flying cloud called the Flying Nimbus only those with pure hearts can ride the cloud. He is capable of transforming into a Great Ape when he is looking at the moon and possesses his tail. Every time Goku resurrects he becomes more powerful.')";
+        String deathstrokeRow = "INSERT INTO " + TABLE_NAME + " ("
+                + COL_ID+ ", "+COL_HERO_NAME + ", " + COL_REAL_NAME + ", "
+                + COL_POWERS + ", " + COL_ADDITIONAL_DETAIL +") Values (null,'Deathstroke','Slade Wilson','Trained Assassin, Marksman, Martial Artist, Enhanced Strength and Reflexes, Limited Healing Factor, Tactical Genius','A man is only as good as his word. But in the underground world of assassins, mercenaries, and guns-for-hire, words mean little. What matters is action and follow through—completing the mission. That’s what makes you good. Never failing—even when it comes to taking down the World’s Greatest Super Heroes, some of the most powerful beings in the world—that’s what makes you the best. That’s what makes you Deathstroke.')";
+
+        db.execSQL(antmanRow);
+        db.execSQL(batmanRow);
         db.execSQL(deadpoolRow);
+        db.execSQL(deathstrokeRow);
+        db.execSQL(gokuRow);
+        db.execSQL(hulkRow);
+        db.execSQL(jokerRow);
+        db.execSQL(kickassRow);
+        db.execSQL(thorRow);
         db.execSQL(wolverineRow);
     }
 
--- NeighborhoodApp/app/src/main/java/ypsitos/neighborhoodapp/TeamActivity.java
+++ NeighborhoodApp/app/src/main/java/ypsitos/neighborhoodapp/TeamActivity.java
@@ -13,15 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class TeamActivity extends AppCompatActivity {
-    ImageView mImageView1,mImageView2,mImageView3,mImageView4;
+    ImageView mImageView1, mImageView2, mImageView3, mImageView4;
     TextView mTextView1, mTextView2, mTextView3, mTextView4;
-    Cursor mCursor1,mCursor2,mCursor3,mCursor4;
+    Cursor mCursor1, mCursor2, mCursor3, mCursor4;
     SQLiteOpenHelper mHelper;
-    Drawable mDeadpoolPic,mWolverinePic,mJokerPic,mBatmanPic,mHulkPic,mKickassPic,mThorPic,mAntmanPic,mGokuPic,mDeathstrokePic;
+    Drawable mDeadpoolPic, mWolverinePic, mJokerPic, mBatmanPic, mHulkPic, mKickassPic, mThorPic, mAntmanPic, mGokuPic, mDeathstrokePic;
     Button backButton;
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor mEditor;
-
 
 
     @Override
@@ -32,38 +31,38 @@ public class TeamActivity extends AppCompatActivity {
         mSharedPreferences = this.getSharedPreferences("prefs", MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
 
-       final int id1 = mSharedPreferences.getInt("id1", -1);
-       final int id2 = mSharedPreferences.getInt("id2",-1);
-        final int id3 = mSharedPreferences.getInt("id3",-1);
-       final int id4 = mSharedPreferences.getInt("id4",-1);
+        final int id1 = mSharedPreferences.getInt("id1", -1);
+        final int id2 = mSharedPreferences.getInt("id2", -1);
+        final int id3 = mSharedPreferences.getInt("id3", -1);
+        final int id4 = mSharedPreferences.getInt("id4", -1);
 
         mCursor1 = mHelper.searchHeroListById(id1);
         mCursor2 = mHelper.searchHeroListById(id2);
         mCursor3 = mHelper.searchHeroListById(id3);
         mCursor4 = mHelper.searchHeroListById(id4);
 
-        mImageView1 = (ImageView)findViewById(R.id.imageView1);
-        mImageView2 = (ImageView)findViewById(R.id.imageView2);
-        mImageView3 = (ImageView)findViewById(R.id.imageView3);
-        mImageView4 = (ImageView)findViewById(R.id.imageView4);
+        mImageView1 = (ImageView) findViewById(R.id.imageView1);
+        mImageView2 = (ImageView) findViewById(R.id.imageView2);
+        mImageView3 = (ImageView) findViewById(R.id.imageView3);
+        mImageView4 = (ImageView) findViewById(R.id.imageView4);
 
         mDeadpoolPic = getResources().getDrawable(R.drawable.deadpool);
         mWolverinePic = getResources().getDrawable(R.drawable.wolverineactual);
         mJokerPic = getResources().getDrawable(R.drawable.joker1);
         mBatmanPic = getResources().getDrawable(R.drawable.batman1);
         mHulkPic = getResources().getDrawable(R.drawable.hulk1);
-       mKickassPic = getResources().getDrawable(R.drawable.kickass1);
+        mKickassPic = getResources().getDrawable(R.drawable.kickass1);
         mThorPic = getResources().getDrawable(R.drawable.thor1);
         mAntmanPic = getResources().getDrawable(R.drawable.antman1);
         mGokuPic = getResources().getDrawable(R.drawable.goku1);
         mDeathstrokePic = getResources().getDrawable(R.drawable.deathstroke1);
 
-        mTextView1 = (TextView)findViewById(R.id.textView1);
-        mTextView2 = (TextView)findViewById(R.id.textView2);
-        mTextView3 = (TextView)findViewById(R.id.textView3);
-        mTextView4 = (TextView)findViewById(R.id.textView4);
+        mTextView1 = (TextView) findViewById(R.id.textView1);
+        mTextView2 = (TextView) findViewById(R.id.textView2);
+        mTextView3 = (TextView) findViewById(R.id.textView3);
+        mTextView4 = (TextView) findViewById(R.id.textView4);
 
-        backButton = (Button)findViewById(R.id.backButton);
+        backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,12 +70,12 @@ public class TeamActivity extends AppCompatActivity {
                 startActivity(toHome);
             }
         });
-        
+
         mImageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toDetail = new Intent(TeamActivity.this,MainActivity.class);
-                toDetail.putExtra("id",id1);
+                Intent toDetail = new Intent(TeamActivity.this, MainActivity.class);
+                toDetail.putExtra("id", id1);
                 startActivity(toDetail);
             }
         });
@@ -84,8 +83,8 @@ public class TeamActivity extends AppCompatActivity {
         mImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toDetail = new Intent(TeamActivity.this,MainActivity.class);
-                toDetail.putExtra("id",id2);
+                Intent toDetail = new Intent(TeamActivity.this, MainActivity.class);
+                toDetail.putExtra("id", id2);
                 startActivity(toDetail);
             }
         });
@@ -93,8 +92,8 @@ public class TeamActivity extends AppCompatActivity {
         mImageView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toDetail = new Intent(TeamActivity.this,MainActivity.class);
-                toDetail.putExtra("id",id3);
+                Intent toDetail = new Intent(TeamActivity.this, MainActivity.class);
+                toDetail.putExtra("id", id3);
                 startActivity(toDetail);
             }
         });
@@ -102,12 +101,12 @@ public class TeamActivity extends AppCompatActivity {
         mImageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toDetail = new Intent(TeamActivity.this,MainActivity.class);
-                toDetail.putExtra("id",id4);
+                Intent toDetail = new Intent(TeamActivity.this, MainActivity.class);
+                toDetail.putExtra("id", id4);
                 startActivity(toDetail);
             }
         });
-        
+
         mImageView1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -139,14 +138,14 @@ public class TeamActivity extends AppCompatActivity {
                 return false;
             }
         });
-        
 
-        if(mSharedPreferences.equals(null)
+
+        if (mSharedPreferences.equals(null)
                 ) {
             Toast.makeText(TeamActivity.this, "You Need To Add Heroes!", Toast.LENGTH_LONG).show();
         }
 
-        if(id1 >= 0) {
+        if (id1 >= 0) {
             mCursor1.moveToFirst();
             mTextView1.setText(mCursor1.getString(mCursor1.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)));
             if (mCursor1.getString(mCursor1.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")) {
@@ -172,7 +171,7 @@ public class TeamActivity extends AppCompatActivity {
             }
         }
 
-        if(id2 >= 0) {
+        if (id2 >= 0) {
             mCursor2.moveToFirst();
             mTextView2.setText(mCursor2.getString(mCursor2.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)));
             if (mCursor2.getString(mCursor2.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")) {
@@ -198,14 +197,14 @@ public class TeamActivity extends AppCompatActivity {
             }
         }
 
-        if(id3 >= 0){
+        if (id3 >= 0) {
             mCursor3.moveToFirst();
             mTextView3.setText(mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)));
-            if(mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")){
+            if (mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")) {
                 mImageView3.setImageDrawable(mDeadpoolPic);
-            }else if(mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Wolverine")){
+            } else if (mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Wolverine")) {
                 mImageView3.setImageDrawable(mWolverinePic);
-            }else if (mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Joker")) {
+            } else if (mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Joker")) {
                 mImageView3.setImageDrawable(mJokerPic);
             } else if (mCursor3.getString(mCursor3.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Batman")) {
                 mImageView3.setImageDrawable(mBatmanPic);
@@ -224,14 +223,14 @@ public class TeamActivity extends AppCompatActivity {
             }
         }
 
-        if(id4 >= 0){
+        if (id4 >= 0) {
             mCursor4.moveToFirst();
             mTextView4.setText(mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)));
-            if(mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")){
+            if (mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Deadpool")) {
                 mImageView4.setImageDrawable(mDeadpoolPic);
-            }else if(mCursor4.getString(mCursor1.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Wolverine")){
+            } else if (mCursor4.getString(mCursor1.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Wolverine")) {
                 mImageView4.setImageDrawable(mWolverinePic);
-            }else if (mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Joker")) {
+            } else if (mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Joker")) {
                 mImageView4.setImageDrawable(mJokerPic);
             } else if (mCursor4.getString(mCursor4.getColumnIndex(SQLiteOpenHelper.COL_HERO_NAME)).contains("Batman")) {
                 mImageView4.setImageDrawable(mBatmanPic);
